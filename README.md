e9aa328ef57923767d771fdbb6bd0010328a5b1e
d-j-craig <122878062+d-j-craig@users.noreply.github.com>
2026-03-07 14:09:25 +0200

    Added Tranco list for testing
    added tranco list to test crawler.
    Also updated folder structure with data folder and for future refactoring.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # domain-checker
-a prototype domain checker, which goes to a web domain and extracts different attributes
+
+Project containing a prototype domain checker, which goes to a web domain and extracts different attributes relating to the site or page.
+
+
+The input to the tool is a csv containing a list of domains as text or strings. The domain nma emust be in the format: 'domain.com'. 
+
+The tools then outputs a CSV containing the domain information.
--- main.go
+++ main.go
@@ -7,35 +7,55 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
-// want to check the status of web domains
-// input: input a list of domains
-// output: domains with status code
+// Check the status of web domains
+// Input: input a list of domains
+// Output: domains with status code
 
 // input type
 type domainList []string
 
 // intermediate type
 type statusList struct {
-	name   string
-	status int
-	err    error
+	name         string
+	status       int
+	responseTime float64
+	err          error
 }
 
 // output type
 type statusDB []statusList
 
 func main() {
+	//only used for performance
+	appStartTime := time.Now()
 
-	domains := readDomainsCsv("domains.csv")
+	fmt.Println("Started: ", appStartTime)
+
+	filename := "./data/input/" + "tranco_list_200.csv"
+
+	domains := readDomainsCsv(filename)
+
+	//only used for performance
+	crawlStartTime := time.Now()
 
 	domains_status := getStatus(domains)
 
-	err := domains_status.saveToOutputCSV("domains_output.csv")
+	//only used for performance
+	crawlElapsedTime := time.Since(crawlStartTime)
+
+	err := domains_status.saveToOutputCSV("./data/output/" + "output_" + filename)
 	if err != nil {
 		fmt.Println("Error saving CSV:", err)
 	}
+
+	//only used for performance
+	appElapsed := time.Since(appStartTime)
+
+	fmt.Println("Total Time: ", appElapsed)
+	fmt.Println("Crawl Time: ", crawlElapsedTime)
 
 }
 
@@ -56,7 +76,7 @@ func readDomainsCsv(filename string) domainList {
 		fmt.Println("Error reading csv:", err)
 	}
 
-	// define the domains as a blank domainList, for each record in the range above append the record to the lsit
+	// define the domains as a blank domainList, for each record in the range above append the record to the list
 	domains := domainList{}
 	for _, record := range records {
 
@@ -78,68 +98,82 @@ func readDomainsCsv(filename string) domainList {
 
 }
 
-// gets status code of website and prints a slice of structs
+// sends http GET request to extract status code and estimate domain response time.
 func getStatus(dl domainList) statusDB {
 
-	statusDB := statusDB{}
+	// create a go channel and create a count variable.
+	c := make(chan statusList)
+	count := len(dl)
 
+	// for each domain in the range take make a request, extract the data, if no errors return the response
 	for _, domain := range dl {
-		// get response from domain with error
-		resp, err := http.Get(domain)
-		// if there is an error status code is 0 and return error message, append this to the statusDB object.
-		if err != nil {
+		go func(domain string) {
 
+			startTime := time.Now()
+
+			resp, err := http.Get(domain)
+
+			elapsed := time.Since(startTime).Seconds()
+
+			// Define status
 			status := statusList{
-				name:   domain,
-				status: 0,
-				err:    err,
+				name:         domain,
+				status:       0,
+				responseTime: 0,
+				err:          err,
 			}
 
-			statusDB = append(statusDB, status)
-			continue
+			// If there are no errors add data to statusList
+			if err == nil {
+				status.status = resp.StatusCode
+				status.responseTime = elapsed
+				resp.Body.Close()
+			}
 
-		}
-		// if no error then extract the details from the domain response and populate the status struct
-		status := statusList{
-			name:   domain,
-			status: resp.StatusCode,
-			err:    err,
-		}
-		// append the status list struct to the statusdb to be returned, close the response body afterwards.
-		statusDB = append(statusDB, status)
-
-		resp.Body.Close()
-
+			// Send to go channel
+			c <- status
+		}(domain)
 	}
 
-	return statusDB
+	// Collect results in a statusDB
+	var results statusDB
+	for range count {
+		status := <-c
+		results = append(results, status)
+	}
 
+	return results
 }
 
 // write statusDB struct to a csv file
 func (sd statusDB) saveToOutputCSV(filename string) error {
+
 	// create a new file to store the data and handle the errors
 	file, err := os.Create(filename)
 	if err != nil {
 		return err
 	}
+
 	// defer closes the file at the end of the main function
 	defer file.Close()
+
 	// creates new csv writer
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
-	// define the headers for the final csv
-	writer.Write([]string{"domain", "status", "error"})
-	// loop through the data in the DB struct and extract the data as strings
 
+	// define the headers for the final csv
+	writer.Write([]string{"domain", "status", "response_time_s", "error"})
+
+	// loop through the data in the DB struct and extract the data as strings
 	for _, status := range sd {
 		statusStr := strconv.Itoa(status.status)
+		statusResponseTime := strconv.FormatFloat(status.responseTime, 'f', 4, 64)
 		errStr := ""
 		if status.err != nil {
 			errStr = status.err.Error()
 		}
 		// writes the final file, each time it does it it adds a row to the csv
-		writer.Write([]string{status.name, statusStr, errStr})
+		writer.Write([]string{status.name, statusStr, statusResponseTime, errStr})
 	}
 
 	return nil
@@ -149,6 +183,6 @@ func (sd statusDB) saveToOutputCSV(filename string) error {
 // add print method to statusDB type
 func (sd statusDB) Print() {
 	for _, status := range sd {
-		fmt.Println(status.name, status.status, status.err)
+		fmt.Println(status.name, status.status, status.responseTime, status.err)
 	}
 }
